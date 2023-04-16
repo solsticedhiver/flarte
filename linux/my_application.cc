d03e4754fd1c5a426f96007d2d5c60083a553b18
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2023-04-16 19:48:40 +0200

    feat: capitalize flarte name in linux/Windows window title bar

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "flarte");
+    gtk_header_bar_set_title(header_bar, "Flarte");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "flarte");
+    gtk_window_set_title(window, "Flarte");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
