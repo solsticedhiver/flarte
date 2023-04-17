ca5aabd2b26f879252f306b2f2cb1f1e868f142a
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2023-04-17 02:04:37 +0200

    feat: implement searching for a video/show

--- lib/search.dart
+++ lib/search.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flarte/detail.dart';
+import 'package:flarte/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({
@@ -11,6 +18,9 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<VideoData> data = [];
+  String search = '';
+
   @override
   void initState() {
     super.initState();
@@ -21,13 +31,130 @@ class _SearchScreenState extends State<SearchScreen> {
     super.dispose();
   }
 
+  void _showDialogProgram(BuildContext context, VideoData v) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+              elevation: 8.0,
+              child: SizedBox(
+                  width: min(MediaQuery.of(context).size.width - 100, 600),
+                  child: ShowDetail(
+                    video: v,
+                  )));
+        });
+  }
+
+  Future<void> _searchTerm(String search) async {
+    String lang = Provider.of<LocaleModel>(context, listen: false)
+        .getCurrentLocale(context)
+        .languageCode;
+    final resp = await http.get(Uri.parse(
+        'https://www.arte.tv/api/rproxy/emac/v4/${lang}/web/pages/SEARCH/?page=1&query=${Uri.encodeComponent(search)}'));
+    final Map<String, dynamic> jr = json.decode(resp.body);
+    final List<dynamic> zones = jr['value']['zones'];
+    for (var z in zones) {
+      if (z['code'] == 'listing_SEARCH') {
+        setState(() {
+          data.clear();
+          data.addAll((z['content']['data'] as List<dynamic>)
+              .map((v) => VideoData.fromJson(v)));
+        });
+        break;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    int count = MediaQuery.of(context).size.width ~/ 285;
+    double width = 285.0 * count;
+    final controller = TextEditingController();
+    Widget results;
+
+    if (data.isNotEmpty) {
+      results = Container(
+          width: width,
+          child: GridView.count(
+            childAspectRatio: 0.85,
+            crossAxisCount: count,
+            children: data.map((v) {
+              return InkWell(
+                  onTap: () {
+                    _showDialogProgram(context, v);
+                  },
+                  child: VideoCard(
+                    video: v,
+                    useSubtitle: false,
+                    withShortDescription: true,
+                    size: CarouselListSize.normal,
+                  ));
+            }).toList(),
+          ));
+    } else {
+      results = const SizedBox.shrink();
+    }
+
+    if (search.isNotEmpty) {
+      controller.text = search;
+    }
+
     return Scaffold(
         appBar: AppBar(title: Text(AppLocalizations.of(context)!.strSearch)),
         body: Center(
-          child: Text(AppLocalizations.of(context)!.strNotImpl),
-        ));
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+              Container(
+                width: width - 10 * 2,
+                margin: const EdgeInsets.only(
+                    top: 16, bottom: 16, left: 10, right: 10),
+                child: Row(children: [
+                  Expanded(
+                      flex: 5,
+                      child: TextField(
+                        controller: controller,
+                        onSubmitted: (value) {
+                          setState(() {
+                            search = controller.text;
+                          });
+                          _searchTerm(search);
+                        },
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          labelText:
+                              AppLocalizations.of(context)!.strSearchTerms,
+                          suffixIcon: IconButton(
+                              focusColor: Theme.of(context).canvasColor,
+                              highlightColor: Theme.of(context).canvasColor,
+                              splashColor: Theme.of(context).canvasColor,
+                              hoverColor: Theme.of(context).canvasColor,
+                              onPressed: () {
+                                controller.clear();
+                              },
+                              icon: const Icon(Icons.backspace)),
+                        ),
+                      )),
+                  const SizedBox(width: 24),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          search = controller.text;
+                        });
+                        _searchTerm(controller.text);
+                      },
+                      child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(AppLocalizations.of(context)!.strSubmit,
+                              style: const TextStyle(fontSize: 20)))),
+                ]),
+              ),
+              Text(AppLocalizations.of(context)!.strResults,
+                  style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: 16),
+              Expanded(flex: 6, child: results),
+            ])));
   }
 }
 
