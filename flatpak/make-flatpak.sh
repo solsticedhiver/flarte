66ab63f099b4911c6402da35a15c2dc9ef7467e4
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2023-04-19 03:31:20 +0200

    build(CI): only install nodejs when run with act
    minor capitalization fix in comment
    
    * [skip ci]

--- flatpak/make-flatpak.sh
+++ flatpak/make-flatpak.sh
@@ -13,12 +13,12 @@ ln -s ../build/linux/x64/${buildtype}/bundle flarte
 echo ':: running flatpak-builder'
 flatpak-builder --ccache --force-clean build-dir io.github.solsticedhiver.flarte.yml
 
-version=`grep ^version ../pubspec.yaml |cut -f 2 -d ' '|cut -f 1 -d '+'`
+#version=`grep ^version ../pubspec.yaml |cut -f 2 -d ' '|cut -f 1 -d '+'`
 sha=`git rev-parse --short main`
 rm -f flarte-*.flatpak
 # build flatpak single-file bundle
 echo ":: making flatpak single-file bundle: flarte-${version}-${sha}-x86_64.flatpak"
 flatpak build-export repo.d build-dir
-flatpak build-bundle repo.d flarte-${version}-${sha}-x86_64.flatpak io.github.solsticedhiver.flarte
+flatpak build-bundle repo.d flarte-${sha}-x86_64.flatpak io.github.solsticedhiver.flarte
 
 rm -rf repo.d
