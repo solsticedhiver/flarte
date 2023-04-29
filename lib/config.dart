231e8628791c967455c8ef73137369d149f4606e
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2023-04-29 12:06:22 +0200

    build: bump release to 0.3.1

--- lib/config.dart
+++ lib/config.dart
@@ -7,7 +7,7 @@ import 'package:path/path.dart' as path;
 
 class AppConfig {
   static const String name = 'Flarte';
-  static const String version = '0.3.0';
+  static const String version = '0.3.1';
   static const String url = 'https://github.com/solsticedhiver/flarte';
   static PlayerTypeName player = PlayerTypeName.embedded;
   // index of resolution, usually 0=>216p, 1=>360p, 2=>432p, 3=> 720p, 4=> 1080p
